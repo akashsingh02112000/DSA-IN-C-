178d5a0a5c70812e3ca5c7cc771fd2b0f376a708
akashsingh02112000 <125112041+akashsingh02112000@users.noreply.github.com>
2023-06-24 11:12:40 +0530

    Update and rename main[1].cpp to ARRAYP3.cpp

--- ARRAY/ARRAYP3.cpp
+++ ARRAY/ARRAYP3.cpp
@@ -1,11 +1,10 @@
-/******************************************************************************
+ /* <aside>
+💡 **Q3.** Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
+You must write an algorithm with O(log n) runtime complexity.
+0*/
 
-******************************************************************************/
+
 #include <iostream>
 #include <vector>
 
