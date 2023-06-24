3277be088864de85e83a7aaacf368b97ff8c182e
akashsingh02112000 <125112041+akashsingh02112000@users.noreply.github.com>
2023-06-24 10:42:06 +0530

    Update and rename main[1].cpp to arrayp2.cpp

--- ARRAY/arrayp2.cpp
+++ ARRAY/arrayp2.cpp
@@ -1,11 +1,17 @@
-/******************************************************************************
+/*<aside>
+💡 **Q1.** Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
-*******************************************************************************/
+You can return the answer in any order.
+
+**Example:**
+Input: nums = [2,7,11,15], target = 9
+Output0 [0,1]
+
+*/
+
+
 #include <iostream>
 #include <vector>
 #include <unordered_map>
